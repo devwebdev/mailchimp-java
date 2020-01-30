c712783e00b92c408941f290f4bddf7127043a7e
Stefano Bianucci <stb@webdev.it>
2020-01-30 14:27:34 +0100

    FN-1026 metodi ordine e prodotto

--- src/test/java/com/mailchimp/integration/OrderTest.java
+++ src/test/java/com/mailchimp/integration/OrderTest.java
@@ -1,4 +1,69 @@
 package com.mailchimp.integration;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import com.mailchimp.domain.customer.Customer;
+import com.mailchimp.domain.order.OrderLine;
+import org.junit.Test;
+
+import com.mailchimp.domain.order.Order;
+import com.mailchimp.domain.order.OrderCreate;
+
 public class OrderTest extends AbstractIntegrationTest {
+
+    @Test
+    public void testCreateOrderShouldMapTowardsMailChimp() throws IOException {
+        // GIVEN
+        Customer customer = new Customer();
+        customer.setId("customer_id");
+        customer.setEmailAddress("email_address");
+        OrderLine orderLine = new OrderLine();
+        orderLine.setId("order_line_id");
+        orderLine.setPrice(2500);
+        OrderCreate order = OrderCreate.builder()
+                .setId("order_id")
+                .setCustomer(customer)
+                .setLines(Collections.singletonList(orderLine)).build();
+
+        expectPost("/3.0/ecommerce/stores/store_id/orders", "order/create.json", "order/create.json");
+        // WHEN
+        Order created = mailChimpClient.create("store_id", order);
+        // THEN
+        assertNotNull(created);
+    }
+
+    @Test
+    public void testDeleteOrderShouldMapTowardsMailChimp() {
+        // GIVEN
+        expectDelete("/3.0/ecommerce/stores/store_id/orders/order_id");
+        // WHEN
+        mailChimpClient.removeOrder("store_id","order_id");
+        // THEN no exception occurs
+    }
+
+
+    @Test
+    public void testUpdateOrderShouldMapTowardsMailChimp() throws IOException {
+        // GIVEN
+        expectPost("/3.0/ecommerce/stores/store_id/orders/order_id", "order/create.json", "order/update.json");
+        // WHEN
+        Customer customer = new Customer();
+        customer.setId("customer_id");
+        customer.setEmailAddress("email_address");
+        OrderLine orderLine = new OrderLine();
+        orderLine.setId("order_line_id");
+        orderLine.setPrice(2500);
+        OrderCreate order = OrderCreate.builder()
+                .setId("order_id")
+                .setCustomer(customer)
+                .setLines(Collections.singletonList(orderLine)).build();
+        Order updated = mailChimpClient.updateOrder("store_id", "order_id", order);
+        // THEN
+        assertNotNull(updated);
+        assert (updated.getLines().get(0).getPrice() == 3000);
+    }
+
 }
--- src/test/java/com/mailchimp/integration/ProductTest.java
+++ src/test/java/com/mailchimp/integration/ProductTest.java
@@ -14,7 +14,7 @@ import com.mailchimp.domain.product.ProductVariant;
 public class ProductTest extends AbstractIntegrationTest {
 
     @Test
-    public void testCreateShouldMapTowardsMailChimp() throws IOException {
+    public void testCreateProductShouldMapTowardsMailChimp() throws IOException {
         // GIVEN
         ProductVariant productVariant = new ProductVariant();
         productVariant.setId("product_variant_id");
@@ -31,4 +31,31 @@ public class ProductTest extends AbstractIntegrationTest {
         assertNotNull(created);
     }
 
+    @Test
+    public void testDeleteShouldMapTowardsMailChimp() {
+        // GIVEN
+        expectDelete("/3.0/ecommerce/stores/store_id/products/product_id");
+        // WHEN
+        mailChimpClient.removeProduct("store_id", "product_id");
+        // THEN
+    }
+
+    @Test
+    public void testUpdateProductShouldMapTowardsMailChimp() throws IOException {
+        // GIVEN
+        expectPatch("/3.0/ecommerce/stores/store_id/products/product_id", "product/create.json", "product/update.json");
+        // WHEN
+        ProductVariant productVariant = new ProductVariant();
+        productVariant.setId("product_variant_id");
+        productVariant.setTitle("product_variant_title");
+        productVariant.setPrice(2990);
+        ProductCreate product = ProductCreate.builder()
+                .setId("product_id")
+                .setTitle("product_title")
+                .setVariants(Collections.singletonList(productVariant)).build();
+        Product updated = mailChimpClient.updateProduct("store_id", "product_id", product);
+        // THEN
+        assertNotNull(updated);
+        assert(updated.getVariants().get(0).getPrice() == 3000);
+    }
 }
